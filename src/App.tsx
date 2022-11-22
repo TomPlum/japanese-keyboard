22a0ec92d44a817342b56306c40e18889e3650f4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-11-22 20:04:23 +0000

    JPUI-44 | Installed saas and testing react keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-simple-keyboard/build/css/index.css';
+import Keyboard from 'react-simple-keyboard';
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.container}>
+          <Keyboard />
+      </div>
     </div>
   );
 }
